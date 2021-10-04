09e4acccdba5b725d44c011164fc8177a9fadcc0
Nazar-Klymenko <52259266+Nazar-Klymenko@users.noreply.github.com>
2021-10-04 08:44:01 +0200

    new ui 404 page

--- frontend/src/pages/Error404.js
+++ frontend/src/pages/Error404.js
@@ -1,16 +1,35 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+import { ContentWrap } from "@components/content";
 
 import { NavLink } from "react-router-dom";
 
 const Error404 = () => {
+  const { t } = useTranslation();
   return (
-    <div className="error404-page">
-      <span>
-        4<span>0</span>4
-      </span>
-      <h1>Sorry, we couln't find this page</h1>
-      <NavLink to="/dashboard">To Dashboard</NavLink>
-    </div>
+    <ContentWrap direction="column" fullWidth>
+      <Container>
+        <Number>404</Number>
+        <h1>{t("404.title")}</h1>
+        <NavLink to="/dashboard">{t("404.link")}</NavLink>
+      </Container>
+    </ContentWrap>
   );
 };
 export default Error404;
+
+const Number = styled.span`
+  color: ${({ theme }) => theme.blue};
+  font-size: 5rem;
+`;
+const Container = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  margin: 20vh auto;
+  a {
+    color: ${({ theme }) => theme.blue};
+  }
+`;

7d745eab26450b9da70cc4d8f8d6dafd58c680eb
Nazar-Klymenko <52259266+Nazar-Klymenko@users.noreply.github.com>
2021-11-08 22:41:30 +0100

    router ux fix

--- admin/src/components/CustomRoutes.jsx
+++ admin/src/components/CustomRoutes.jsx
@@ -13,27 +13,32 @@ export const PrivateRoute = ({ component: Component, ...rest }) => {
     <Route
       {...rest}
       render={(props) => {
-        if (isSendingRequest && isLoggedIn) {
+        if (isSendingRequest) {
           return <Loader />;
         }
-        if (isLoggedIn && !isApproved) {
-          return (
-            <Redirect
-              to={{
-                pathname: "/buffer",
-                state: { from: props.location },
-              }}
-            />
-          );
-        }
-        if (isLoggedIn) {
-          return <Component {...props} />;
-        } else {
-          return (
-            <Redirect
-              to={{ pathname: "/auth/login", state: { from: props.location } }}
-            />
-          );
+        if (!isSendingRequest) {
+          if (isLoggedIn && !isApproved) {
+            return (
+              <Redirect
+                to={{
+                  pathname: "/buffer",
+                  state: { from: props.location },
+                }}
+              />
+            );
+          }
+          if (isLoggedIn) {
+            return <Component {...props} />;
+          } else {
+            return (
+              <Redirect
+                to={{
+                  pathname: "/auth/login",
+                  state: { from: props.location },
+                }}
+              />
+            );
+          }
         }
       }}
     />

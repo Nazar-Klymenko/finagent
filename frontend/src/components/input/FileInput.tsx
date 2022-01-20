f080dfac802188c15508de28c00a9411d44814fe
Nazar-Klymenko <52259266+Nazar-Klymenko@users.noreply.github.com>
2022-01-20 10:24:46 +0100

    feat: file input

--- frontend/src/components/input/FileInput.tsx
+++ frontend/src/components/input/FileInput.tsx
@@ -1,20 +1,22 @@
-import React, { FC, useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import { useTranslation } from "next-i18next";
 import Image from "next/image";
 
-import { Typography } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { IconButton, Typography } from "@mui/material";
+import { styled, css } from "@mui/material/styles";
 import _ from "lodash";
 import { useDropzone } from "react-dropzone";
 import { Controller, useFormContext } from "react-hook-form";
-
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import DeleteIcon from "@mui/icons-material/Delete";
 import {
   InputContainer,
   InputErrorMessage,
   Label,
   Optional,
 } from "./LocalStyles";
+import formatBytes from "@helpers/formatBytes";
 
 interface Props extends InputProps {
   placeholder?: string;
@@ -66,101 +68,159 @@ const FileInput = ({
 export default FileInput;
 
 const Dropzone = ({ value, onChange }: any): JSX.Element => {
-  const [files, setFiles] = useState(value);
+  const { t } = useTranslation();
+  const [files, setFiles] = useState<any>(value || []);
 
-  useEffect(() => {
-    setFiles(value);
-  }, [value]);
-
-  const onDrop = useCallback((acceptedFiles) => {
-    setFiles(
-      acceptedFiles.map((file: any) =>
-        Object.assign(file, {
-          preview: URL.createObjectURL(file),
-        })
-      )
-    );
-  }, []);
-  const { getRootProps, getInputProps } = useDropzone({
-    onDrop,
-    accept: "image/jpeg, image/png",
-  });
-
-  useEffect(
-    () => () => {
-      files?.forEach((file: any) => URL.revokeObjectURL(file.preview));
+  console.log(files);
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      let newFiles = [
+        ...files,
+        ...acceptedFiles.map((file: any) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          })
+        ),
+      ];
+      setFiles(newFiles);
+      onChange(newFiles);
     },
-    [files]
+    [files, onChange]
   );
+
+  // useEffect(() => {
+  //   return () =>
+  //     files?.forEach((file: any) => URL.revokeObjectURL(file.preview));
+  // }, [files]);
+
+  const { getRootProps, getInputProps, isDragActive, isFocused, isDragReject } =
+    useDropzone({
+      onDrop,
+      accept: "image/jpeg, image/png",
+    });
+
+  const removeFile = (file: File) => () => {
+    const newFiles = [...files];
+    newFiles.splice(newFiles.indexOf(file), 1);
+    setFiles(newFiles);
+    onChange(newFiles);
+    //@ts-ignore
+    URL.revokeObjectURL(file.preview);
+  };
 
   const thumbs =
     files?.length > 0 &&
     files.map((file: any, idx: number) => (
-      <div key={file.name}>
-        <Image src={file.preview} alt={file.name} height={80} width={64} />
-        <Typography>
-          {file.name}
-          {idx}
-        </Typography>
-        <Typography
-          onClick={() => {
-            //@ts-ignore
-            setFiles((files) => files.splice(idx, 1));
-          }}
-        >
-          Remove
-        </Typography>
-      </div>
+      <ImagePreview key={idx}>
+        <ImageWrap>
+          <Image
+            src={file.preview}
+            alt={file.name}
+            height={100}
+            width={120}
+            objectFit="cover"
+          />
+        </ImageWrap>
+        <ImageNameContainer>
+          <ImageName
+            sx={{ typography: { sm: "body1", xs: "body2" } }}
+            variant="body1"
+          >
+            {_.truncate(file.name, {
+              length: 18,
+              omission:
+                "..." + file.name.substr(file.name.lastIndexOf(".") + 1),
+            })}
+          </ImageName>
+          <Typography variant="body2">{formatBytes(file.size)}</Typography>
+        </ImageNameContainer>
+
+        <IconButton sx={{ mx: "0.5rem" }} onClick={removeFile(file)}>
+          <DeleteIcon />
+        </IconButton>
+      </ImagePreview>
     ));
 
   return (
     <section>
-      <Base {...getRootProps()}>
-        <input
-          {...getInputProps({
-            onChange: (e) => onChange(e.target.files),
-          })}
-        />
-        <div>Drag and drop your images here.</div>
+      <Base {...getRootProps({ isFocused, isDragActive, isDragReject })}>
+        <input {...getInputProps()} />
+        <FileUploadIcon sx={{ mx: "auto" }}></FileUploadIcon>
+        <Typography variant="body2" align="center">
+          {t("Form.Inputs.dropzone")}{" "}
+          <a href="#">{t("Form.Inputs.dropzoneBrowse")}</a>
+        </Typography>
       </Base>
-      <aside>{thumbs}</aside>
+      <Aside>{thumbs}</Aside>
     </section>
   );
 };
 
-const Base = styled("div")<{ isDragActive: boolean }>`
+const Aside = styled("aside")`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const ImagePreview = styled("div")`
+  border-radius: ${({ theme }) => theme.shape.borderRadius}px;
+  display: flex;
+  border: 1px solid ${({ theme }) => theme.palette.divider};
+  width: 100%;
+  align-items: center;
+  margin-top: 12px;
+  overflow: hidden;
+`;
+const ImageWrap = styled("div")`
+  min-height: 100px;
+  min-width: 120px;
+  max-height: 100px;
+  max-width: 120px;
+  ${({ theme }) => theme.breakpoints.down("sm")} {
+    min-height: 80px;
+    min-width: 100px;
+    max-height: 80px;
+    max-width: 100px;
+  }
+`;
+const ImageNameContainer = styled("div")`
+  flex: 1;
+  padding: 0 1rem;
+  max-width: 100%;
+`;
+const ImageName = styled(Typography)`
+  overflow: hidden;
+  text-overflow: ellipsis;
+`;
+
+const Base = styled("div", {
+  shouldForwardProp: (prop) =>
+    prop !== "isFocused" && prop !== "isDragActive" && prop !== "isDragReject",
+})<{
+  isFocused?: boolean;
+  isDragActive: boolean;
+  isDragReject: boolean;
+}>`
+  cursor: pointer;
   display: flex;
   flex-direction: column;
-  align-items: center;
-  padding: 20px;
+  place-content: center center;
+  padding: 16.5px 14px;
   border-radius: 4px;
   background-color: #fafafa;
   color: #bdbdbd;
-  /* transition: border 0.3s ease-in-out; */
-
-  border: 1px dashed ${({ theme }) => theme.palette.grey.A100};
+  border: 1px dashed ${({ theme }) => theme.palette.grey[500]};
+  height: 102px;
   &:hover {
-    border: 1px dashed ${({ theme }) => theme.palette.grey.A700};
+    border: 1px dashed ${({ theme }) => theme.palette.grey[900]};
   }
 
   ${({ isDragActive }) =>
     isDragActive &&
-    `  border-color: #2196f3
-`}
+    css`
+      border: 1px dashed #2196f3 !important;
+    `}
+  ${({ isDragReject }) =>
+    isDragReject &&
+    css`
+      border: 1px dashed red !important;
+    `}
 `;
-
-// isDragActive,
-//     isDragAccept,
-//     isDragReject
-
-{
-  /* <OutlinedInput
-  key={name}
-  onChange={field.onChange}
-  value={field.value}
-  id={name}
-  autoComplete={autoComplete}
-  error={!!_.get(errors, name)}
-  {...other}
-/>; */
-}

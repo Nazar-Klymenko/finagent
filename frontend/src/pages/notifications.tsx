f1b12c4be9ec68f8ad023bfbdab09db80ee313e0
Nazar Klymenko <nazar.klymenko187@gmail.com>
2022-06-06 16:42:20 +0200

    feat: notifications page

--- frontend/src/pages/notifications.tsx
+++ frontend/src/pages/notifications.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from "next";
+import { useTranslation } from "next-i18next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import Head from "next/head";
 import Image from "next/image";
 
@@ -6,12 +8,26 @@ import { Typography } from "@mui/material";
 
 import withAuth from "@helpers/withAuth";
 
+import { PageContainer } from "@components/layout";
+
 const Notifications: NextPage = () => {
+  const { t } = useTranslation();
+
   return (
-    <Typography variant="h4" textAlign="center">
-      Notifications
-    </Typography>
+    <PageContainer title={t("Pages.notifications")}>
+      <Typography variant="h4" textAlign="center">
+        Notifications
+      </Typography>
+    </PageContainer>
   );
 };
 
 export default withAuth(Notifications);
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+}
